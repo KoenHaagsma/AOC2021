715f8bb70a831d5b79c5e5ae957a0b0ac2cec356
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-12-07 19:46:06 +0100

    Second part

--- days/day 1/index.js
+++ days/day 1/index.js
@@ -18,7 +18,7 @@ function formatData(data) {
 function checkPrevious(data) {
     let count = 0;
     console.log(data.length);
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 1; i < data.length; i++) {
         if (data[i] > data[i - 1]) {
             console.log(`${data[i]} (increased)`);
             count += 1;
@@ -36,10 +36,14 @@ function checkPrevious(data) {
 // console.log(checkPrevious(formatData(fetchValues())));
 
 function addNextThree(data) {
+    let sum = [];
     for (let i = 0; i < data.length; i++) {
-        let sum = data[i] + data[i + 1] + data[i + 2] + data[i + 3];
-        console.log(sum);
+        if (i + 2 < data.length) {
+            sum.push(data[i] + data[i + 1] + data[i + 2]);
+        }
     }
+    return sum;
 }
 
-console.log(addNextThree(formatData(fetchValues())));
+// Last part value
+console.log(checkPrevious(addNextThree(formatData(fetchValues()))));
