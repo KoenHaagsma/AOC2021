b2824f553a359e1dd68a541294e4734c0738704d
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-12-08 11:52:14 +0100

    Day 2 result

--- days/day_2/index.js
+++ days/day_2/index.js
@@ -1,9 +1,5 @@
-// Read file input
 const fs = require('fs');
 const path = require('path');
-
-let horizontalPosition;
-let depth;
 
 function fetchValues() {
     try {
@@ -14,4 +10,53 @@ function fetchValues() {
     }
 }
 
-function formatData(data) {}
+function formatData(data) {
+    const array = [];
+    const formattedData = data.split('\r\n');
+    formattedData.forEach((el) => array.push(el.split(' ')));
+    return array;
+}
+
+function position(data) {
+    let horizontalPosition = 0;
+    let depth = 0;
+
+    data.forEach((el) => {
+        if (el[0] === 'forward') {
+            horizontalPosition += parseInt(el[1]);
+        } else if (el[0] === 'up') {
+            depth -= parseInt(el[1]);
+        } else if (el[0] === 'down') {
+            depth += parseInt(el[1]);
+        }
+    });
+
+    console.log(`Horizontal position: ${horizontalPosition}`);
+    console.log(`Depth: ${depth}`);
+    return horizontalPosition * depth;
+}
+
+// First part
+// console.log(position(formatData(fetchValues())));
+
+function complicatedPosition(data) {
+    let horizontalPosition = 0;
+    let depth = 0;
+    let aim = 0;
+
+    data.forEach((el) => {
+        if (el[0] === 'forward') {
+            horizontalPosition += parseInt(el[1]);
+            depth += aim * el[1];
+        } else if (el[0] === 'up') {
+            aim -= parseInt(el[1]);
+        } else if (el[0] === 'down') {
+            aim += parseInt(el[1]);
+        }
+    });
+
+    return horizontalPosition * depth;
+}
+
+// Second part
+console.log(complicatedPosition(formatData(fetchValues())));
